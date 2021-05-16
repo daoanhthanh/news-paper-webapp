0fe9294c18f820e0cdea2c8e3897d70c0fcfdab5
@nhThanh <daoanhthanh.work@gmail.com>
2021-05-16 07:00:10 +0700

    final product

--- client/consumeapi.js
+++ client/consumeapi.js
@@ -5,7 +5,11 @@
 // xmlhttp.open("GET", baseurl+"/all", true);
 // var articles = JSON.parse(xmlhttp.responseText);
 
-
+var articlePo;
+var articleCovid;
+var articleBusiness;
+var articleTech;
+var articleScience;
 
 async function getNews(category) {
     console.log(category)
@@ -17,42 +21,46 @@ async function getNews(category) {
     return articles;
 }
 
-
-getNews("Politic").then(articles => console.log(articles[2].title))
-
-
-// console.log(data)
-
-
-const politic = document.querySelector("#ourserv");
-console.log(politic);
-for(let i=0; articles.length; i++){
-    let article = document.createElement('article');
-    article.class = 'politic';
-
-
-    let h1 = document.createElement('h1');
-    h1.textContent = "he";
-    let img = document.createElement('img');
-    img.src = 'images/1.jpg';
-    let p = document.createElement('p');
-    let img_div = document.createElement('div');
-
-    let a = document.createElement('a');
-
-    img_div.class = 'rm';
-    a.textContent = 'Read More';
-    a.href = 'fullwidth.html';
-    img_div.appendChild(a);
-
-    article.appendChild(h1);
-    article.appendChild(img);
-    article.appendChild(p);
-    article.appendChild(a);
-    politic.appendChild(article);
-
-
+async function renderNews(category){
+    var news = await getNews(category);
+    console.log(news);
 }
+
+async function po(){
+    const politic = document.querySelector("#ourserv");
+    console.log(politic);
+    for(let i=0; 4; i++){
+        let article = document.createElement('article');
+        article.class = 'politic';
+
+
+        let h1 = document.createElement('h1');
+        h1.textContent = "he";
+        let img = document.createElement('img');
+        img.src = 'images/1.jpg';
+        let p = document.createElement('p');
+        let img_div = document.createElement('div');
+
+        let a = document.createElement('a');
+
+        img_div.class = 'rm';
+        a.textContent = 'Read More';
+        a.href = 'fullwidth.html';
+        img_div.appendChild(a);
+
+        article.appendChild(h1);
+        article.appendChild(img);
+        article.appendChild(p);
+        article.appendChild(a);
+        politic.appendChild(article);
+
+
+    }
+}
+
+renderNews("Politic");
+po();
+
 
 
 

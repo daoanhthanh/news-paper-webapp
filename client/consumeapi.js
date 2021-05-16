b52087f27f47f8be57b502c56ca914ec09645a33
@nhThanh <daoanhthanh.work@gmail.com>
2021-05-16 09:48:11 +0700

    dat

--- client/consumeapi.js
+++ client/consumeapi.js
@@ -10,44 +10,51 @@ var articleCovid;
 var articleBusiness;
 var articleTech;
 var articleScience;
+let article_array;
+let articles;
 
 async function getNews(category) {
-    console.log(category)
+    // console.log(category)
     // http://dataservice.accuweather.com/forecasts/v1/daily/1day/353412?categorykey=xI8DEqgQCyLKgiwVvS6jT2KryVg13v6t&language=vi-vn
     const base = await `http://localhost:1900/article/all?category=${category}`;
     let response = await fetch(base);
 
-    let articles = await response.json();
-    return articles;
+    articles = await response.json();
+
 }
 
-async function renderNews(category){
-    var news = await getNews(category);
-    console.log(news);
-}
 
-async function po(){
+
+// const politic = document.querySelector("#ourserv");
+async function po() {
     const politic = document.querySelector("#ourserv");
-    console.log(politic);
-    for(let i=0; 4; i++){
+    await getNews('Politic');//dược r
+    console.log(articles);
+    for (let i = 0; i < articles.length; i++) {
         let article = document.createElement('article');
         article.class = 'politic';
 
+        //thé bảo chưa gọi ở đâu :))
+        // t bịk lỗi ở đau thế?
+        //không gọi trong áync function thì nó bị pengà, hiểu r c <3 OK i//ng mãi
+
 
         let h1 = document.createElement('h1');
-        h1.textContent = "he";
+        h1.textContent = articles[i].title;
         let img = document.createElement('img');
         img.src = 'images/1.jpg';
         let p = document.createElement('p');
+        p.textContent = articles[i].shortIntro;
+
+
         let img_div = document.createElement('div');
-
         let a = document.createElement('a');
-
         img_div.class = 'rm';
         a.textContent = 'Read More';
         a.href = 'fullwidth.html';
-        img_div.appendChild(a);
 
+
+        img_div.appendChild(a);
         article.appendChild(h1);
         article.appendChild(img);
         article.appendChild(p);
@@ -58,9 +65,60 @@ async function po(){
     }
 }
 
-renderNews("Politic");
+// const covid = document.querySelector("#ourserv");
+// console.log(politic);
+// console.log(getNews("Politic").length);
+
+async function co() {
+    const covid = document.querySelector("#covid");
+    // console.log(politic);
+    // article_array = await getNews("Politic");
+    // console.log(article_array);
+    await getNews('Covid-19');//dược r
+    console.log(articles);
+    for (let i = 0; i < articles.length; i++) {
+        let article = document.createElement('article');
+        article.class = 'covid';
+
+        //thé bảo chưa gọi ở đâu :))
+        // t bịk lỗi ở đau thế?
+        //không gọi trong áync function thì nó bị pengà, hiểu r c <3 OK i//ng mãi
+
+
+        let h1 = document.createElement('h1');
+        h1.textContent = articles[i].title;
+        let img = document.createElement('img');
+        img.src = 'images/pbs2.jpg';
+        let p = document.createElement('p');
+        p.textContent = articles[i].shortIntro;
+
+
+        let img_div = document.createElement('div');
+        let a = document.createElement('a');
+        img_div.class = 'rm';
+        a.textContent = 'Read More';
+        a.href = 'fullwidth.html';
+
+        
+        img_div.appendChild(a);
+        article.appendChild(h1);
+        article.appendChild(img);
+        article.appendChild(p);
+        article.appendChild(a);
+        covid.appendChild(article);
+
+
+    }
+}
+
 po();
+co();
+
+// renderNews("Politic");
 
 
 
 
+
+//t đang gọi c đáy
+//chỗ t đang ồn k nghe được
